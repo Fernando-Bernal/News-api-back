d70a938eb76c2c6a70d913105a19e57c3e126380
Fernando Bernal <97192959+Fernando-Bernal@users.noreply.github.com>
2025-05-30 08:36:56 -0300

    configuracion completa eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,27 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+      indent: ["error", 2],
+      "no-unused-vars": "warn",
+      eqeqeq: ["error", "always"],
+      "no-console": "off"
+    }
+  },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: "readonly",
+      },
+    },
+  },
 ]);
